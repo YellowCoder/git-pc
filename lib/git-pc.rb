23274c95ae8652761f7e43c17fe1f94a3bedb742
Adriano Tadao <drianotadao@gmail.com>
2016-05-04 22:30:09 -0300

    Added the -S param

--- lib/git-pc.rb
+++ lib/git-pc.rb
@@ -3,43 +3,44 @@ require 'grit'
 require 'git-pc/version'
 
 class GitPc
-  attr_accessor :argv
-
-  def run(argv)
+  def initialize(argv)
     @argv = argv
+    handle_args
+  end
 
+  private
+
+  def run
     status_command
     add_command
     commit_command
     push_command
   end
 
-  private
-
   def status_command
-    puts "Executing status command".green
-    puts "git status".yellow
+    puts 'Executing status command'.green
+    puts 'git status'.yellow
     puts
     %x(git status)
   end
 
   def push_command
-    puts "Executing push command".green
+    puts 'Executing push command'.green
     puts "git push origin #{ current_branch }".yellow
     puts
     %x(git push origin #{ current_branch })
   end
 
   def commit_command
-    puts "Executing commit command".green
-    puts "git commit -m #{ message }".yellow
+    puts 'Executing commit command'.green
+    puts "git commit -S -m #{ message }".yellow
     puts
-    %x(git commit -m "#{ message }")
+    %x(git commit -S -m "#{ message }")
   end
 
   def add_command
-    puts "Executing add command".green
-    puts "git add .".yellow
+    puts 'Executing add command'.green
+    puts 'git add .'.yellow
     puts
     %x(git add .)
   end
@@ -62,6 +63,31 @@ class GitPc
       @repo = Grit::Repo.new(repo_dir)
     rescue
       raise "We don't seem to be in a git repository."
+    end
+  end
+
+  def help_message
+    help = <<HELP
+  Shortcut to create a commit and push them.
+      $ git pc 'message commit' # create a commit and push them
+      $ git up --version        # print version info
+      $ git up --help           # print this message
+HELP
+    help
+  end
+
+  def handle_args
+    case @argv
+    when []
+      puts '** You need to pass some arg **'.red
+      exit
+    when ['-v'], ['--version']
+      puts "git-pc #{ GitPc::VERSION }"
+      exit
+    when ['-h'], ['--help']
+      puts help_message.yellow
+    else
+      run
     end
   end
 end
--- lib/git-pc/version.rb
+++ lib/git-pc/version.rb
@@ -1,3 +1,3 @@
 class GitPc
-  VERSION = "0.0.1"
+  VERSION = '0.1.1'
 end
